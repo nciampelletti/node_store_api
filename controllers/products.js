e4750c69fa00f24d44e7182138ca117eec739bd2
nciampelletti <natashat@shaw.ca>
2022-08-08 14:05:48 -0700

    setup getallproducts fil\ters

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,29 @@
 import Product from "../models/product.js"
 
 export const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
+  const search = "accent"
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  })
   res.status(200).json({ products, nbHits: products.length })
 }
 
 export const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false
+  }
+
+  if (company) {
+    queryObject.company = company
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }
+  }
+
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
